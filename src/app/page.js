17cc7b32fd1cca6d35d21e5a1d18226460bb6b55
Rania <raniaattar1616@gmail.com>
2025-04-01 15:54:23 +0200

    modif RANIA

--- src/app/page.js
+++ src/app/page.js
@@ -2,18 +2,20 @@ export default function Home() {
   return (
     <div>
       Voici les 8 contributeurs du projet :
+      <main>
       <ol>
-<<<<<<< HEAD
+
         <li>Alex</li>
-=======
-    < main>
+
+    
         <li>Abdrahamane</li>
         <li>Alex</li>
         <li>Alex fraudeur</li>
         <li>stephane honnète citoyen</li>
-    <main>
->>>>>>> b158f4865f2b30544963af2355da28ad73683de4
+    
+
       </ol>
+      </main>
     </div>
   );
 }
