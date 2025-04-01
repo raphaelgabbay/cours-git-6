5a290f37aebe1f8f1307a9a1806cdbc8c001c7e6
KONATE-ABDRAHAMANE <abdrahamanekonate738@gmail.com>
2025-04-01 14:45:32 +0100

    modification Abdrahamane

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,9 @@ export default function Home() {
     <div>
       Voici les 8 contributeurs du projet :
       <ol>
+        <li>Abdrahamane</li>
         <li>Alex</li>
+        
       </ol>
     </div>
   );
